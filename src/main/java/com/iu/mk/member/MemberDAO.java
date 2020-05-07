bf7b9f9fa6c6a64c2828dd07a9799620f6f4dd97
thdus3009 <thdus3009@gmail.com>
2020-05-07 18:11:50 +0900

    Add Annotation repository

--- src/main/java/com/iu/mk/member/MemberDAO.java
+++ src/main/java/com/iu/mk/member/MemberDAO.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MemberDAO {
 
 	@Autowired
