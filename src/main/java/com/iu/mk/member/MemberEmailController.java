9e16fbe9028c4c23caf81964cc70aa7a5180a9aa
thdus3009 <thdus3009@gmail.com>
2020-05-27 12:30:27 +0900

    mail send

--- src/main/java/com/iu/mk/member/MemberEmailController.java
+++ src/main/java/com/iu/mk/member/MemberEmailController.java
@@ -5,9 +5,11 @@ import java.io.PrintWriter;
 import java.util.Random;
 import java.util.logging.Logger;
 
+import javax.inject.Inject;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,22 +26,66 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value="/member/**")
 public class MemberEmailController {
 
-	@Autowired
-	private MemberService memberService;
-	JavaMailSender mailSender;  
-	
-    //로깅을 위한 변수 - 디버깅(에러찾아줌.?)
-   // private static final Logger logger= (Logger) LoggerFactory.getLogger(MemberEmailController.class);
-    private static final String String = null;
+	   @Inject    //서비스를 호출하기 위해서 의존성을 주입
+	    JavaMailSender mailSender;     //메일 서비스를 사용하기 위해 의존성을 주입함.
+	    MemberService memberservice; //서비스를 호출하기 위해 의존성을 주입
+	    
+	    private static final String String = null;
 	
 	
 	
 	//injeungEmail
 	@PostMapping("injeungEmail.do")
-	public void injeungEmail(ModelAndView mv, MemberVO memberVO)throws Exception{
+	public ModelAndView injeungEmail(ModelAndView mv, MemberVO memberVO,String email)throws Exception{
 		
-		
-		
+	       Random r = new Random();
+           int dice = r.nextInt(4589362) + 49311; //이메일로 받는 인증코드 부분 (난수)
+           
+           String setfrom = "thdus3009@gmail.com";
+           String tomail = email; // 받는 사람 이메일
+           String title = "회원가입 인증 이메일 입니다."; // 제목
+           String content =
+           
+           System.getProperty("line.separator")+ //한줄씩 줄간격을 두기위해 작성
+           
+           System.getProperty("line.separator")+
+                   
+           "안녕하세요 회원님 저희 홈페이지를 찾아주셔서 감사합니다"
+           
+           +System.getProperty("line.separator")+
+           
+           System.getProperty("line.separator")+
+   
+           " 인증번호는 " +dice+ " 입니다. "
+           
+           +System.getProperty("line.separator")+
+           
+           System.getProperty("line.separator")+
+           
+           "받으신 인증번호를 홈페이지에 입력해 주시면 다음으로 넘어갑니다."; // 내용
+           
+           
+           try {
+               MimeMessage message = mailSender.createMimeMessage();
+               MimeMessageHelper messageHelper = new MimeMessageHelper(message,
+                       true, "UTF-8");
+
+               messageHelper.setFrom(setfrom); // 보내는사람 생략하면 정상작동을 안함
+               messageHelper.setTo(tomail); // 받는사람 이메일
+               messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
+               messageHelper.setText(content); // 메일 내용
+               
+               mailSender.send(message);
+           } catch (Exception e) {
+               System.out.println(e);
+           }
+           
+           //ModelAndView로 보낼 페이지를 지정하고, 보낼 값을 지정한다.
+			  mv.addObject("result","인증메일을 보냈습니다."); 
+			  mv.addObject("path","memberJoin");
+			  mv.setViewName("common/result");
+           
+           return mv;
 	}
 	
 	
